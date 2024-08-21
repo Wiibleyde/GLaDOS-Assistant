6b8c8ad81dceadba7613364156d66e8a1e8ee31a
Wiibleyde <nathan@bonnell.fr>
2024-08-21 02:23:10 +0200

    chore: Update talk command to send messages to specific users
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- src/commands/general/talk.ts
+++ src/commands/general/talk.ts
@@ -1,25 +1,41 @@
 import { CommandInteraction, EmbedBuilder, SlashCommandBuilder } from "discord.js"
-import { errorEmbed, successEmbed } from "../../utils/embeds"
+import { errorEmbed, successEmbed } from "@/utils/embeds"
 
 export const data = new SlashCommandBuilder()
     .setName("talk")
-    .setDescription("Parler avec le bot")
-    .addStringOption(option => 
+    .setDescription("Parler avec en utilisant bot")
+    .addStringOption(option =>
         option
             .setName("message")
             .setDescription("Le message à envoyer")
             .setRequired(true)
-        )
+    )
+    .addStringOption(option =>
+        option
+            .setName("mp")
+            .setDescription("ID de la personne")
+            .setRequired(false)
+    )
 
 export async function execute(interaction: CommandInteraction) {
     const firstResponse = await interaction.deferReply({ ephemeral: true, fetchReply: true })
-    const channel = interaction.channel
     const message = interaction.options.get("message")?.value as string
+    const member = interaction.options.get("mp")?.value as string
 
-    if (!channel) {
-        await interaction.editReply({ embeds: [errorEmbed(interaction, new Error("Impossible de trouver le salon de discussion"))] })
-        return
+    if (member) {
+        const user = await interaction.client.users.fetch(member)
+        if (!user) {
+            await interaction.editReply({ embeds: [errorEmbed(interaction, new Error("Impossible de trouver l'utilisateur"))] })
+            return
+        }
+        await user.send(message)
+    } else {
+        const channel = interaction.channel
+        if (!channel) {
+            await interaction.editReply({ embeds: [errorEmbed(interaction, new Error("Impossible de trouver le salon de discussion"))] })
+            return
+        }
+        await channel.send(message)
     }
-    await channel.send(message)
     await interaction.editReply({ embeds: [successEmbed(interaction, "Message envoyé")] })
 }
