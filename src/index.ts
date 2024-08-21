f8e698ad2a27888defbc049beee25534e69169ce
Wiibleyde <nathan@bonnell.fr>
2024-08-21 03:24:07 +0200

    chore: Add modals to handle custom interactions
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import { Client } from "discord.js"
 import { deployCommands, deployDevCommands } from "./deploy-commands"
 import { config } from "./config"
-import { commands, devCommands } from "./commands"
+import { commands, devCommands, modals } from "./commands"
 import { logger } from "./utils/logger"
+// import { CronJob } from 'cron';
 
 export const client = new Client({
     intents: ["Guilds", "GuildMessages", "DirectMessages"],
@@ -17,17 +18,24 @@ client.once("ready", () => {
 })
 
 client.on("interactionCreate", async (interaction) => {
-    if (!interaction.isCommand()) {
-        return
+    if (interaction.isCommand()) {
+        const { commandName } = interaction;
+        if (commands[commandName as keyof typeof commands]) {
+            commands[commandName as keyof typeof commands].execute(interaction)
+        }
+        if (devCommands[commandName as keyof typeof devCommands]) {
+            devCommands[commandName as keyof typeof devCommands].execute(interaction)
+        }
+    } else if (interaction.isModalSubmit()) {
+        if (modals[interaction.customId as keyof typeof modals]) {
+            modals[interaction.customId as keyof typeof modals](interaction)
+        }
     }
-    const { commandName } = interaction;
-    if (commands[commandName as keyof typeof commands]) {
-        commands[commandName as keyof typeof commands].execute(interaction)
-    }
-    if (devCommands[commandName as keyof typeof devCommands]) {
-        devCommands[commandName as keyof typeof devCommands].execute(interaction)
-    }
-
 })
+
+// const job = new CronJob('0 0 0 * * *', () => {
+//     logger.info('Cron job every day at midnight')
+//     // Add your cron job code here
+// })
 
 client.login(config.DISCORD_TOKEN)
