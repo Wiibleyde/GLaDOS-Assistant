89fda24f7888327569af50ab336a2c03303dafd3
wiibleyde <nathan@bonnell.fr>
2024-11-26 10:49:23 +0100

    Mise à jour de la journalisation des rôles utilisateur pour afficher les détails au format JSON
    Signed-off-by: wiibleyde <nathan@bonnell.fr>

--- src/commands/dev/debug.ts
+++ src/commands/dev/debug.ts
@@ -85,7 +85,7 @@ export async function execute(interaction: CommandInteraction): Promise<void> {
         }
     }
     const userRoles = await interaction.guild?.members.fetch(interaction.user.id).then(async member => await member.roles.cache)
-    logger.debug(`User roles: ${userRoles}`)
+    logger.debug(`User roles: ${JSON.stringify(userRoles)}`)
     if (role && userRoles?.has(role.id)) {
         await server?.members.cache.get(interaction.user.id)?.roles.remove(role)
         await interaction.editReply({ content: `Vous n'êtes plus en mode debug sur le serveur ${server?.name}` })
