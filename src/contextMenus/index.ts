cca30c7c6189ef37133cd9af0da3de84c2ad6f84
wiibleyde <nathan@bonnell.fr>
2024-12-17 10:43:44 +0100

    feat: add user banner retrieval to context menus
    Signed-off-by: wiibleyde <nathan@bonnell.fr>

--- src/contextMenus/index.ts
+++ src/contextMenus/index.ts
@@ -1,13 +1,15 @@
 import * as quote from "./message/quote"
 
 import * as profilePicture from "./user/profilePicture"
+// import * as banner from "./user/banner"
 
 const contextMessageMenus = {
     "Créer un citation": quote
 }
 
 const contextUserMenus = {
-    "Récupèrer la photo de profil": profilePicture
+    "Récupèrer la photo de profil": profilePicture,
+    // "Récupèrer la bannière": banner
 }
 
 
--- src/contextMenus/user/banner.ts
+++ src/contextMenus/user/banner.ts
@@ -1,19 +1,26 @@
+import { errorEmbed } from "@/utils/embeds";
 import { ApplicationCommandType, ContextMenuCommandBuilder, EmbedBuilder, UserContextMenuCommandInteraction } from "discord.js";
 
 export const data: ContextMenuCommandBuilder = new ContextMenuCommandBuilder()
-    .setName("Récupèrer la photo de profil")
+    .setName("Récupèrer la bannière")
     //@ts-expect-error - This is a valid type
-    .setType(ApplicationCommandType.User)
+    .setType(ApplicationCommandType.User) // ApplicationCommandType.User
 
 export async function execute(interaction: UserContextMenuCommandInteraction): Promise<void> {
     await interaction.deferReply({ ephemeral: true });
 
     const author = interaction.targetUser
-    const userProfilePicture = author?.displayAvatarURL({ extension: "png", size: 1024 })
+
+    const userBanner = author?.bannerURL({ size: 1024, extension: "png" })
+
+    if (!userBanner) {
+        await interaction.editReply({ embeds: [errorEmbed(interaction, new Error("L'utilisateur n'a pas de bannière."))] })
+        return
+    }
 
     const embed = new EmbedBuilder()
-        .setTitle("Photo de profil")
-        .setImage(userProfilePicture)
+        .setTitle("Bannière")
+        .setImage(userBanner)
         .setColor(0x00ff00)
         .setTimestamp()
         .setFooter({ text: `Eve – Toujours prête à vous aider.`, iconURL: interaction.client.user.displayAvatarURL() })
