41eb8039cfffae1458ab56e9ec18f17b731a42c2
wiibleyde <nathan@bonnell.fr>
2024-12-19 16:45:31 +0100

    feat: update version to 1.2.0 and reorganize button imports in interactions
    Signed-off-by: wiibleyde <nathan@bonnell.fr>

--- src/interactions/buttons/index.ts
+++ src/interactions/buttons/index.ts
@@ -1,7 +1,3 @@
-import { backButton } from "../commands/music/back";
-import { loopButton } from "../commands/music/loop";
-import { resumeAndPauseButton } from "../commands/music/pause";
-import { skipButton } from "../commands/music/skip";
 import { changeRadio } from "./hope/changeRadio";
 import { handleAddRadio } from "./hope/handleAddRadio";
 import { handleRemoveRadio } from "./hope/handleRemoveRadio";
@@ -9,15 +5,21 @@ import { handleMotusTry } from "./motus/handleMotusTry";
 import { handleQuizButton } from "./quiz/handleQuizButton";
 import { reportQuestionButton } from "./quiz/reportQuestionButton";
 
+import { backButton } from "../commands/music/back";
+import { loopButton } from "../commands/music/loop";
+import { resumeAndPauseButton } from "../commands/music/pause";
+import { skipButton } from "../commands/music/skip";
+
 export const buttons = {
     handleQuizButton,
     reportQuestionButton,
     changeRadio,
+    handleMotusTry,
+    handleAddRadio,
+    handleRemoveRadio,
+
     backButton,
     resumeAndPauseButton,
     skipButton,
     loopButton,
-    handleMotusTry,
-    handleAddRadio,
-    handleRemoveRadio,
 }
